34b0e77a3938f7dbdd7bb6a235848b2df0aa64a4
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-09-13 00:39:16 +0700

    v1.3

--- lib/omniauth/strategies/zalo.rb
+++ lib/omniauth/strategies/zalo.rb
@@ -10,21 +10,32 @@ module OmniAuth
         site: 'https://oauth.zaloapp.com',
         authorize_url: '/v3/auth',
         token_url: '/v3/access_token',
-        token_method: :get
+        token_method: :get,
       }
 
-      uid { raw_info['userId'] }
+      option :provider_ignores_state, true
+
+      uid { raw_info['id'] }
 
       info do
         {
-          name:        raw_info['displayName'],
-          image:       raw_info['pictureUrl'],
-          description: raw_info['statusMessage']
+          name:  raw_info['name'],
+          image: raw_info['picture']['data']['url'],
         }
       end
 
       def authorize_params
         super.merge(app_id: self.options.client_id)
+      end
+
+      def build_access_token
+        token_url_params = {app_id: options.client_id, app_secret: options.client_secret, code: request.params['code'], redirect_uri: callback_url}.merge(token_params.to_hash(:symbolize_keys => true))
+        parsed_response = client.request(options.client_options.token_method, client.token_url(token_url_params), parse: :json).parsed
+        hash = {
+          :access_token => parsed_response["access_token"],
+          :expires_in => parsed_response["expires_in"],
+        }
+        ::OAuth2::AccessToken.from_hash(client, hash)
       end
 
       alias :old_callback_url :callback_url
@@ -37,18 +48,8 @@ module OmniAuth
         end
       end
 
-      def callback_path
-        params = session['omniauth.params']
-
-        if params.nil? || params['callback_url'].nil?
-          super
-        else
-          URI(params['callback_url']).path
-        end
-      end
-
       def raw_info
-        @raw_info ||= JSON.load(access_token.get('https://graph.zalo.me/v2.0/me').body)
+        @raw_info ||= JSON.load(access_token.get("https://graph.zalo.me/v2.0/me?access_token=#{access_token.token}&fields=id,birthday,name,gender,picture,phone").body)
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
       end
